32853a7c998f8baef9ef6fe52b4119fd014a7742
Aramdhann <radityamochamad@gmail.com>
2022-11-15 09:02:06 +0700

    add icon title

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="icon" href="./assets/img/icon-pens.png">
     <!-- bootstrap -->
     <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
