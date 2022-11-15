88c65e0381778ac5da4a8ff381a332fcef593e0b
Aramdhann <radityamochamad@gmail.com>
2022-11-15 08:45:57 +0700

    update connection

--- core/connection.php
+++ core/connection.php
@@ -1,12 +1,12 @@
 <?php
-// $dbhost = 'localhost';
-// $dbuser = 'smar_smartpju';
-// $dbpass = 'pensJOSS123';
-// $dbname = 'smar_smartpju'; // sesuaikan nama database
 $dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'smart_pju'; // sesuaikan nama database
+$dbuser = 'smar_smartpju';
+$dbpass = 'pensJOSS123';
+$dbname = 'smar_smartpju'; // sesuaikan nama database
+// $dbhost = 'localhost';
+// $dbuser = 'root';
+// $dbpass = '';
+// $dbname = 'smart_pju'; // sesuaikan nama database
 
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
