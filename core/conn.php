b5d163f3d1f664b0c53ae9b8d6b301c208926171
Aramdhann <radityamochamad@gmail.com>
2022-11-28 15:53:04 +0700

    change conn

--- core/conn.php
+++ core/conn.php
@@ -1,12 +1,12 @@
 <?php
-// $dbhost = 'localhost';
-// $dbuser = 'smar_smart';
-// $dbpass = 'pensJOSS123';
-// $dbname = 'smar_smart'; // sesuaikan nama database
 $dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'smart_pju'; // sesuaikan nama database
+$dbuser = 'smar_smart';
+$dbpass = 'pensJOSS123';
+$dbname = 'smar_smart'; // sesuaikan nama database
+// $dbhost = 'localhost';
+// $dbuser = 'root';
+// $dbpass = '';
+// $dbname = 'smart_pju'; // sesuaikan nama database
 
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
