5c548bfd4e0b1ba9d9c1f278327a1805abe0b10f
Aramdhann <radityamochamad@gmail.com>
2022-11-28 18:33:06 +0700

    updated connection respon, temp, and daya

--- core/connection.php
+++ core/connection.php
@@ -20,15 +20,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($api_key == $api_key_value) {
         $val_lamp = test_input($_POST["val_lamp"]);
         $val_volt = test_input($_POST["val_volt"]);
+        $val_daya = test_input($_POST["val_daya"]);
+        $val_temp = test_input($_POST["val_temp"]);
         $val_amp = test_input($_POST["val_amp"]);
         $val_ldr = test_input($_POST["val_ldr"]);
         $longitude = test_input($_POST["longitude"]);
         $latitude = test_input($_POST["latitude"]);
 
-        $sql = "INSERT INTO tbl_sensor (val_lamp, val_volt, val_amp, val_ldr, latitude, longitude) VALUES ('" . $val_lamp . "', '" . $val_volt . "', '" . $val_amp . "', '" . $val_ldr . "', '" . $latitude . "', '" . $longitude . "')";
+        $sql = "INSERT INTO tbl_sensor (val_lamp, val_volt, val_daya, val_temp, val_amp, val_ldr, latitude, longitude) VALUES ('" . $val_lamp . "', '" . $val_volt . "', '" . $val_daya . "', '" . $val_temp . "', '" . $val_amp . "', '" . $val_ldr . "', '" . $latitude . "', '" . $longitude . "')";
+
+        // get upper
+        $get_upper = mysqli_query($connect, "SELECT up_threshold FROM threshold ORDER BY id DESC LIMIT 1");
+        $data = mysqli_fetch_array($get_upper);
+        $up_threshold = $data['up_threshold'];
+
+        // get lower
+        $get_lower = mysqli_query($connect, "SELECT low_threshold FROM threshold ORDER BY id DESC LIMIT 1");
+        $data = mysqli_fetch_array($get_lower);
+        $low_threshold = $data['low_threshold'];
 
         if ($connect->query($sql) === TRUE) {
-            echo "new record created successfully";
+            echo "new record created successfully with upper threshold: $up_threshold and lower threshold: $low_threshold ";
         } else {
             echo "Error: " . $sql . "<br>" . $connect->error;
         }
