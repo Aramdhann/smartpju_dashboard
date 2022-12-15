d9cb9817a4c00ce5ed03c0b438f3e82f10e8823b
Aramdhann <radityamochamad@gmail.com>
2022-12-15 12:36:29 +0700

    update l-navbar auto show

--- index.php
+++ index.php
@@ -17,13 +17,13 @@
 
 </head>
 
-<body id="body-pd">
-    <header class="header" id="header">
-        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+<body id="body-pd" class="body-pd">
+    <header class="header body-pd" id="header">
+        <div class="header_toggle"> <i class='bx bx-menu bx-x' id="header-toggle"></i> </div>
         <div class="text-title fw-bold">Monitoring Smart PJU</div>
         <a href="./login.php" class="btn btn-light" style="border: 1px solid black;">Admin Login</a>
     </header>
-    <div class="l-navbar" id="nav-bar">
+    <div class="l-navbar show" id="nav-bar">
         <nav class="nav">
             <div>
                 <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
